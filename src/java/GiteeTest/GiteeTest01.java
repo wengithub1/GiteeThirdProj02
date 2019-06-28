af4883f4286d744b3dc12e009c420267a6da328d
wengeone <2374603499@qq.com>
2019-06-28 16:22:34 +0800

    test01

--- src/java/GiteeTest/GiteeTest01.java
+++ src/java/GiteeTest/GiteeTest01.java
@@ -4,5 +4,7 @@ public class GiteeTest01 {
     public static void main(String[] args) {
         System.out.println("gitee test");
         System.out.println("java test01");
+        System.out.println("github test01");
+        System.out.println("github test02");
     }
 }
