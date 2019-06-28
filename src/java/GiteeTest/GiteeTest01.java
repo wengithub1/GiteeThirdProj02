264e4d7d333d0b838ae7485298852d363913cbae
wengeone <2374603499@qq.com>
2019-06-28 15:45:42 +0800

    test01

--- src/java/GiteeTest/GiteeTest01.java
+++ src/java/GiteeTest/GiteeTest01.java
@@ -3,5 +3,6 @@ package java.GiteeTest;
 public class GiteeTest01 {
     public static void main(String[] args) {
         System.out.println("gitee test");
+        System.out.println("java");
     }
 }
